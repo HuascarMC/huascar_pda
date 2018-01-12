2168e4eae43faf49d74358d5c90bdd0e2c8c3282
Huascar <huascarmc@hotmail.com>
2018-01-12 14:09:14 +0000

    numbers update display on click

--- week13/unit_integration_test_b/js_calculator_start_point/tests/integration/tests.js
+++ week13/unit_integration_test_b/js_calculator_start_point/tests/integration/tests.js
@@ -10,10 +10,28 @@ describe('calculator functionality', function() {
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  it('should have working number buttons', function() {
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  })
+
+  // Do the number buttons work to update the display of the running total?
+  it('should update display with number buttons', function() {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number3')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('23')
+  })
+
+  // Do each of the arithmetical operations work to update the display with the result of the operation?
+  it('should update display with result of operation add', function() {
+   running_total = element(by.css('#running_total'))
+   element(by.css('#number7')).click();
+   element(by.css('#operator_add')).click();
+   element(by.css('#number2')).click();
+   element(by.css('#operator_equals')).click();
+   expect(running_total.getAttribute('value')).to.eventually.equal('14')
   })
 
 });
